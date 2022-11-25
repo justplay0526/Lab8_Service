c80f22593a09c2bc215da51a1641bde9b99d3884
justplay0526 <justplay0526@gmail.com>
2022-11-25 08:46:55 +0800

    first version

---
+++ app/src/main/java/com/example/lab8_service/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.lab8_service;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn_service = findViewById(R.id.btn_service);
+        btn_service.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,MyService.class);
+                startService(intent);
+                Toast.makeText(MainActivity.this,"啟動Service", Toast.LENGTH_LONG).show();
+                finish();
+            }
+        });
+    }
+}
